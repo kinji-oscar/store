25c62d1d9dde6f4d16ddd405ecc06b81d0d102cf
younes akkab <142669523+kinji-oscar@users.noreply.github.com>
2023-10-24 22:35:28 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,41 @@
+
+// let money ;
+// //get moneyfrom our wallet
+// var wallet = localStorage.getItem("money");
+// if(wallet === undefined || NaN){
+//     // there is no money in our wallet
+//     money = 0
+// }else{
+//     // there is money in our wallet
+//     money = parseInt(wallet) 
+// }
+// function showMoney(money){
+//     var message= "$"+money;
+//     document.getElementById('msgContainer').innerHTML = message
+//     // set new money in our wallet
+//     localStorage.setItem("money", parseInt(money));     
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
